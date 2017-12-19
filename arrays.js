80c4b0075e96687c31dfe01094d47cf994fa24e3
Abraham Nieto <elmeromero.an@gmail.com>
2017-12-19 12:01:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -40,6 +40,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  var newArray = array.slice(0, array.lenght - 1)
-  return newArray
+  //var newArray = array.slice(0, array.length - 1)
+  return array.slice(0, array.length - 1)
 }
