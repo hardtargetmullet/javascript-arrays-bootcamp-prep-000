9792122eb2aaf74c3d038f3e117c7467bf554e68
Abraham Nieto <elmeromero.an@gmail.com>
2017-12-19 11:39:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,7 +7,7 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-    array.unshift(element)
+   return array.unshift(element)
 }
 
 function addElementToEndOfArray(array, element) {
@@ -17,5 +17,5 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-    array.push(element)
+    return array.push(element)
 }
