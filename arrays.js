3c345f81d382df5479aeb1a60e60b2f1cb3c5460
Abraham Nieto <elmeromero.an@gmail.com>
2017-12-19 11:35:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,17 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element) {
+
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+    array.unshift(element)
+}
+
+function addElementToEndOfArray(array, element) {
+ 
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+    array.push(element)
+}
