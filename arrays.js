dfa0c70ec8c2f12418b05c383f02bf9469c93d76
Abraham Nieto <elmeromero.an@gmail.com>
2017-12-19 12:08:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
-   // var newArray = [element, ...array]
-    
     return [element, ...array]
 }
 
